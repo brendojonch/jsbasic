05d6f90c3a53e47d21a2514924c0f52ed681f43a
brendojonch <brendo.jonch@gmail.com>
2021-07-20 04:45:30 -0300

    att

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-var i = 0;
+const listContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
 
-while (i < 10) {
-    console.log(i);
-    i++;
+
+let lists = []
+// ao receber submit a pagina e atualizada. 
+
+newListForm.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const listName = newListInput.value
+
+    if (listName === null || listName === '') return
+    const list = createList(listName)
+    newListInput.value = null
+    lists.push(list)
+    render()
+})
+
+function createList(name) {
+    return { id: Date.now().toString(), name: name }
+
 }
+
+function render() {
+    clearElement(listContainer)
+    //funçao callback anonim
+    lists.forEach(function (list) {
+        const item = document.createElement('li') //criando no html
+        item.classList.add('item')
+        item.innerText = list.name
+        listContainer.appendChild(item)
+    })
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+
+
+render()
